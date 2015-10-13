f409542f7d1afb54668ad68ea9ff34b83c4e5c59
mguida22 <michael.guida@colorado.edu>
2015-10-12 19:24:52 -0600

    initial commit

---
+++ README.md
@@ -0,0 +1,15 @@
+# Degrees to Directions
+
+Convert degrees into directions. Useful for windspeed, etc.
+
+```js
+var d2d = require('degrees-to-directions');
+
+d2d(0); // "N"
+d2d(10); // "N"
+d2d(45); // "ENE"
+d2d(180); // "S"
+```
+
+### License
+MIT
---
+++ lib.js
@@ -0,0 +1,42 @@
+module.exports = function(d) {
+  if (typeof d !== 'number') {
+    return -1;
+  }
+  
+  // keep within the range: 0 <= d < 360
+  d = d % 360;
+
+  if (11.25 <= d && d < 33.75) {
+    return "NNE";
+  } else if (33.75 <= d && d < 56.25) {
+    return "ENE";
+  } else if (56.25 <= d && d < 78.75) {
+    return "E";
+  } else if (78.75 <= d && d < 101.25) {
+    return "ESE";
+  } else if (101.25 <= d && d < 123.75) {
+    return "ENE";
+  } else if (123.75 <= d && d < 146.25) {
+    return "SE";
+  } else if (146.25 <= d && d < 168.75) {
+    return "SSE";
+  } else if (168.75 <= d && d < 191.25) {
+    return "S";
+  } else if (191.25 <= d && d < 213.75) {
+    return "SSW";
+  } else if (213.75 <= d && d < 236.25) {
+    return "SW";
+  } else if (236.25 <= d && d < 258.75) {
+    return "WSW";
+  } else if (258.75 <= d && d < 281.25) {
+    return "W";
+  } else if (281.25 <= d && d < 303.75) {
+    return "WNW";
+  } else if (303.75 <= d && d < 326.25) {
+    return "NW";
+  } else if (326.25 <= d && d < 348.75) {
+    return "ENE";
+  } else {
+    return "N";
+  }
+};
